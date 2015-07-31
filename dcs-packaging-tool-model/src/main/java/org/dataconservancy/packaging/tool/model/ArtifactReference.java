288464842db8653afcc4d0b8be587fac9504013e
jrmartino <jrm@jhu.edu>
2015-07-31 09:23:09 -0400

    javadoc

--- dcs-packaging-tool-model/src/main/java/org/dataconservancy/packaging/tool/model/ArtifactReference.java
+++ dcs-packaging-tool-model/src/main/java/org/dataconservancy/packaging/tool/model/ArtifactReference.java
@@ -17,36 +17,55 @@
 package org.dataconservancy.packaging.tool.model;
 
 import java.io.File;
-import java.net.URI;
-import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- *  A class to implement the functionality we need for a PackageArtifact's artifactRef. We sometimes need this to have a
- *  String,  URI aspect. This class localizes the translations required, to obviate conversions in calling classes.
- *  Constructor taking a String requires that the String be a valid relative URI. Additional constructors may be added in the future;
- *  all constructors should ensure that every getter will return a valid instance of its type. The getRefURI method takes
- *  the content root directory (or its URI) as an argument, and returns an absolute URI for the artifact reference, resolved against the content root.
+ *  A class to implement the functionality we need for a PackageArtifact's artifactRef.
+ *
  */
 public class ArtifactReference {
 
+    /**
+     * The fragment is the specifier generated for a virtual artifact. When this is not null
+     * the reference does not point to an actual file system entity
+     */
     private String fragment = null;
+
+    /**
+     * The String representing the artifact reference
+     */
     private String refPath;
 
     public ArtifactReference(String refPath) {
        this.refPath=refPath;
     }
 
+    /**
+     *
+     * @param contentRootFile The Content Root directory. This is the parent of the
+     *                        package's root artifact location.
+     * @return A String representing the absolute path pointing to the artifact's File
+     */
     public String getResolvedAbsoluteRefString(File contentRootFile) {
         File absFile = new File(contentRootFile, getRefString());
         return absFile.getPath();
     }
 
+    /**
+     *
+     * @param contentRootFile The Content Root directory. This is the parent of the
+     *                        package's root artifact
+     * @return The Path pointing to the artifact's File
+     */
     public Path getResolvedAbsoluteRefPath(File contentRootFile) {
         return Paths.get(contentRootFile.getPath(), getRefString());
     }
 
+    /**
+     * Accessor for the refString.
+     * @return  The String representing the artifact reference
+     */
     public String getRefString(){
         if (fragment == null || fragment.isEmpty()){
             return refPath;
@@ -55,18 +74,35 @@ public class ArtifactReference {
         }
     }
 
+    /**
+     * A convenience method to return a Path for the artifact Reference
+     * @return Path for the artifact Reference
+     */
     public String getRefPath(){
         return refPath;
     }
 
+    /**
+     * Setter for the fragment specifier
+     * @param fragment the specifier
+     */
     public void setFragment(String fragment){
         this.fragment = fragment;
     }
 
+    /**
+     * Accessor for the specifier fragment
+     * @return The specifier fragment
+     */
     public String getFragment(){
         return this.fragment;
     }
 
+    /**
+     * A convenience method to return just the last path component
+     * plus any specifier
+     * @return
+     */
     public String getRefName(){
         File refFile = new File(refPath);
         if (fragment == null || fragment.isEmpty()){
