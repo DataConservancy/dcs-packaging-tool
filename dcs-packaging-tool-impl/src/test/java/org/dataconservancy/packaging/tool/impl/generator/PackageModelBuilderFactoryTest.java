01009e95c1dd10899938455285a076e4304258f9
bbrosius <benbrosius@gmail.com>
2015-07-31 08:32:10 -0400

    Fixed test failure.

--- dcs-packaging-tool-impl/src/test/java/org/dataconservancy/packaging/tool/impl/generator/PackageModelBuilderFactoryTest.java
+++ dcs-packaging-tool-impl/src/test/java/org/dataconservancy/packaging/tool/impl/generator/PackageModelBuilderFactoryTest.java
@@ -22,7 +22,9 @@ import org.dataconservancy.packaging.tool.api.generator.PackageModelBuilder;
 import org.dataconservancy.packaging.tool.model.GeneralParameterNames;
 import org.dataconservancy.packaging.tool.model.PackageDescription;
 import org.dataconservancy.packaging.tool.model.PackageGenerationParameters;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -34,13 +36,16 @@ import static junit.framework.Assert.assertNull;
 @ContextConfiguration(locations = {"classpath*:/test-applicationContext.xml"})
 public class PackageModelBuilderFactoryTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void testGetNewBuilder() throws InstantiationException, IllegalAccessException {
         PackageDescription desc = new PackageDescription();
         desc.setPackageOntologyIdentifier(OntologyIdentifiers.DCSBO.toString());
         PackageGenerationParameters params = new PackageGenerationParameters();
         params.addParam(GeneralParameterNames.PACKAGE_FORMAT_ID, PackagingFormat.BOREM.toString());
-
+        params.addParam(GeneralParameterNames.CONTENT_ROOT_LOCATION, folder.getRoot().getPath());
         PackageModelBuilder builder = PackageModelBuilderFactory.newBuilder(desc, params);
 
         assertNotNull(builder);
