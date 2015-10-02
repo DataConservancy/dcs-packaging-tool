792f169c998b2656f1d5ac551638f483e7607a24
bbrosius <benbrosius@gmail.com>
2015-10-02 11:23:58 -0400

    Added new methods to the DomainProfileService API for the editing of packages.

--- dcs-packaging-tool-api/src/main/java/org/dataconservancy/packaging/tool/api/DomainProfileService.java
+++ dcs-packaging-tool-api/src/main/java/org/dataconservancy/packaging/tool/api/DomainProfileService.java
@@ -8,6 +8,8 @@ import org.dataconservancy.packaging.tool.model.dprofile.NodeTransform;
 import org.dataconservancy.packaging.tool.model.dprofile.PropertyConstraint;
 import org.dataconservancy.packaging.tool.model.dprofile.PropertyType;
 import org.dataconservancy.packaging.tool.model.dprofile.PropertyValue;
+import org.dataconservancy.packaging.tool.model.ipm.ComparisonNode;
+import org.dataconservancy.packaging.tool.model.ipm.FileInfo;
 import org.dataconservancy.packaging.tool.model.ipm.Node;
 
 /**
@@ -23,36 +25,36 @@ public interface DomainProfileService {
     /**
      * Add a property to a node.
      * 
-     * @param node
-     * @param value
+     * @param node The node that the property will be added to.
+     * @param value The value of the property that should be added to the node.
      */
     void addProperty(Node node, PropertyValue value);
 
     /**
      * Remove a particular property from a node.
      * 
-     * @param node
-     * @param value
+     * @param node The node to remove the properties from.
+     * @param value The value of the property to remove.
      */
     void removeProperty(Node node, PropertyValue value);
 
     /**
      * Remove all properties of a given type from a node.
      * 
-     * @param node
-     * @param type
+     * @param node The node to remove properties from.
+     * @param type The type of property that should be removed from the node.
      */
     void removeProperty(Node node, PropertyType type);
 
     /**
-     * @param node
+     * @param node The node whose properties should be returned.
      * @return All properties of a node.
      */
     List<PropertyValue> getProperties(Node node);
 
     /**
-     * @param node
-     * @param type
+     * @param node The node to get the properties for.
+     * @param type The type of the property that should be retrieved for the node.
      * @return All properties of a node of a certain type.
      */
     List<PropertyValue> getProperties(Node node, PropertyType type);
@@ -61,7 +63,7 @@ public interface DomainProfileService {
      * Check that all the properties on a node satisfy constraints for that node
      * type.
      * 
-     * @param node
+     * @param node The node whose properties are going to be valided.
      * @return valid or invalid
      */
     boolean validateProperties(Node node);
@@ -74,7 +76,7 @@ public interface DomainProfileService {
     /**
      * Format property value as a string according to its type and hint.
      * 
-     * @param value
+     * @param value The value of the property to format.
      * @return Formatted property value
      */
     String formatPropertyValue(PropertyValue value);
@@ -83,8 +85,8 @@ public interface DomainProfileService {
      * Attempt to parse a string into a property value according to its type and
      * hint.
      * 
-     * @param type
-     * @param value
+     * @param type The type of the property that's going to be parsed.
+     * @param value The value of the property to be parsed.
      * @return value on success and null on failure
      */
     PropertyValue parsePropertyValue(PropertyType type, String value);
@@ -93,13 +95,13 @@ public interface DomainProfileService {
      * Transform a node. The tree must be valid before a transform and will be
      * valid after.
      * 
-     * @param node
-     * @param trans
+     * @param node The node that is going to be transformed.
+     * @param trans The node transform to perform on the passed in node.
      */
     void transformNode(Node node, NodeTransform trans);
 
     /**
-     * @param node
+     * @param node The node to get the available transforms for.
      * @return All node transforms able to be performed on the node.
      */
     List<NodeTransform> getNodeTransforms(Node node);
@@ -108,8 +110,8 @@ public interface DomainProfileService {
      * Check if a tree satisfies all its node and optionally property
      * constraints.
      * 
-     * @param root
-     * @param check_properties
+     * @param root The root node of the tree to validate.
+     * @param check_properties True if properties should be validated, false if only the tree structure and types should be validated
      * @return valid or invalid
      */
     boolean validateTree(Node root, boolean check_properties);
@@ -124,7 +126,7 @@ public interface DomainProfileService {
      * On success, domain objects will be created if they do not exist or
      * updated with the new type if they do.
      * 
-     * @param node
+     * @param node The root node of the tree to assign types to.
      * @return success or failure
      */
     boolean assignNodeTypes(Node node);
@@ -133,7 +135,7 @@ public interface DomainProfileService {
      * Create a tree from the file system. The tree has file data associated
      * with it, but does not have types assigned to nodes.
      * 
-     * @param path
+     * @param path The path on the file system to create a node tree from.
      * @return root of tree
      */
     Node createTreeFromFileSystem(Path path);
@@ -148,18 +150,50 @@ public interface DomainProfileService {
      * If a node is marked as not ignored, all descendants and ancestors are
      * also marked as not ignored.
      * 
-     * @param node
-     * @param status
+     * @param node The node to change the ignored status for.
+     * @param status The new status of the node.
      */
     void ignoreNode(Node node, boolean status);
-    
-    
+
+    /**
+     * Checks that the file backing the node is accessible on the file system.
+     * Note that this method makes no assertion that the file or folder pointed to was the original one, just that something exists
+     * at the file location stored in the nodes file info.
+     * @param node The node to check if it's file info file location is still accessible.
+     * @return true if the file is accessible false otherwise.
+     */
+    boolean checkFileInfoIsAccessible(Node node);
+
+    /**
+     * TODO: This may not be needed as one could just update the file info object on the node directly.
+     *
+     * Updates the FileInfo object on the node, when the file or folder backing the node has changed.
+     * @param node The node to update the file information for.
+     * @param info The new file info to set on the node.
+     */
+    void updateFileInfo(Node node, FileInfo info);
+
     /**
      * TODO: Needed? Is this done automatically?
      * 
      * Copy inheritable properties from this node to descendants.
      * 
-     * @param node
+     * @param node The node to propagate properties to from it's parent.
      */
     void propagateInheritedProperties(Node node);
+
+    /**
+     * Compares the three provided under the existing tree root node with the tree under the comparison tree root node.
+     * @param existingTree The root node of the existing tree to compare.
+     * @param comparisonTree The root node of the new tree to compare against the existing tree.
+     * @return The root node of the of the tree showing the results of the comparison.
+     */
+    ComparisonNode compareTree(Node existingTree, Node comparisonTree);
+
+    /**
+     * Merges the provided comparison tree into the existing tree.
+     * @param existingTree The existing tree that will recieve the results of the merge.
+     * @param comparisonTree The comparison tree to merge into the existing tree.
+     */
+    void mergeTree(Node existingTree, ComparisonNode comparisonTree);
 }
