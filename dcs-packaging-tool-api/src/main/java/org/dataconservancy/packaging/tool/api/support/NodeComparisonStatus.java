f4e1dc0acc04067cb6caa8f36ddcc58a077451a5
bbrosius <benbrosius@gmail.com>
2015-10-05 15:35:38 -0400

    Updated IPMService to use map instead of ComparisonNode as discussed in todays meeting. Got rid of ComparisonNode, create NodeComparisonStatus enum because I had no idea where to put it. Will live on it's own for now.

--- dcs-packaging-tool-api/src/main/java/org/dataconservancy/packaging/tool/api/support/NodeComparisonStatus.java
+++ dcs-packaging-tool-api/src/main/java/org/dataconservancy/packaging/tool/api/support/NodeComparisonStatus.java
@@ -1,6 +1,9 @@
 package org.dataconservancy.packaging.tool.api.support;
 
-
+/**
+ * Enum for storing the status of node comparisons when trees are compared.
+ * //TODO: This can likely be moved into some service instead of living on it's own.
+ */
 public enum NodeComparisonStatus {
     ADDED,
     DELETED,
