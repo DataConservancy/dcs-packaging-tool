f669c13219a250ae47089a883bf1358cca8826d7
bbrosius <benbrosius@gmail.com>
2015-10-21 08:03:43 -0400

    Removed file attributes class from FarmIpmTree updated to use new constructor and setter methods. Filled out more info when FileInfo references a file.

--- dcs-packaging-tool-impl/src/test/java/org/dataconservancy/packaging/tool/impl/FarmIpmTree.java
+++ dcs-packaging-tool-impl/src/test/java/org/dataconservancy/packaging/tool/impl/FarmIpmTree.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.dataconservancy.packaging.tool.model.ipm.FileInfo;
 import org.dataconservancy.packaging.tool.model.ipm.FileInfo.Algorithm;
@@ -25,84 +26,39 @@ public class FarmIpmTree {
         this.root = new Node(URI.create("test:farm"));
 
         root.setNodeType(profile.getFarmNodeType());
-        root.setFileInfo(createFileInfo("/farm", -1, -1, 100));
+        root.setFileInfo(createFileInfo("/farm", "farm", -1, -1, 100, false));
 
         Node barn = new Node(URI.create("test:barn1"));
         barn.setNodeType(profile.getBarnNodeType());
-        barn.setFileInfo(createFileInfo("/farm/barn1", -1, 200, 400));
+        barn.setFileInfo(createFileInfo("/farm/barn1", "barn1", -1, 200, 400, false));
         
         Node cow = new Node(URI.create("test:cow1"));
         cow.setNodeType(profile.getCowNodeType());
-        cow.setFileInfo(createFileInfo("/farm/barn1/cow1", -1, 600, 600));
+        cow.setFileInfo(createFileInfo("/farm/barn1/cow1", "cow1", -1, 600, 600, false));
         
         Node video = new Node(URI.create("test:cow1_video"));
         video.setNodeType(profile.getMediaNodeType());
-        video.setFileInfo(createFileInfo("/farm/barn1/cow1/lastgoodbye.mp4", 10000000, 800, 1000));
+        video.setFileInfo(createFileInfo("/farm/barn1/cow1/lastgoodbye.mp4", "lastgoodbye.mp4", 10000000, 800, 1000, true));
         
         root.addChild(barn);
         barn.addChild(cow);
         cow.addChild(video);
     }
-    
-    // TODO Modify FileInfo not to use BasicFileAttributes?
-    
-    private FileInfo createFileInfo(String path, final long size, final long create_date_ms, final long mod_date_ms) {
-        BasicFileAttributes attrs = new BasicFileAttributes() {
-            @Override
-            public long size() {
-                return size;
-            }
 
-            @Override
-            public FileTime lastModifiedTime() {
-                return FileTime.fromMillis(mod_date_ms);
-            }
+    private FileInfo createFileInfo(String path, String name, final long size, final long create_date_ms, final long mod_date_ms, boolean file) {
+        FileInfo fileInfo = new FileInfo(Paths.get(path).toUri(), name);
+        fileInfo.setSize(size);
+        fileInfo.setCreationTime(FileTime.fromMillis(create_date_ms));
+        fileInfo.setLastModifiedTime(FileTime.fromMillis(mod_date_ms));
+        fileInfo.setIsFile(file);
+        fileInfo.setIsDirectory(!file);
 
-            @Override
-            public FileTime lastAccessTime() {
-                return FileTime.fromMillis(mod_date_ms);
-            }
-
-            @Override
-            public boolean isSymbolicLink() {
-                // TODO Auto-generated method stub
-                return false;
-            }
-
-            @Override
-            public boolean isRegularFile() {
-                // TODO Auto-generated method stub
-                return false;
-            }
-
-            @Override
-            public boolean isOther() {
-                // TODO Auto-generated method stub
-                return false;
-            }
-
-            @Override
-            public boolean isDirectory() {
-                // TODO Auto-generated method stub
-                return false;
-            }
-
-            @Override
-            public Object fileKey() {
-                // TODO Auto-generated method stub
-                return null;
-            }
-
-            @Override
-            public FileTime creationTime() {
-                return FileTime.fromMillis(create_date_ms);
-            }
-        };
-
-        List<String> formats = new ArrayList<String>();
-        Map<Algorithm, String> checksums = new HashMap<>();
-
-        return new FileInfo(Paths.get(path), attrs, formats, checksums);
+        if (file) {
+            fileInfo.addFormat("application/octet-stream");
+            fileInfo.addChecksum(Algorithm.MD5, UUID.randomUUID().toString());
+            fileInfo.addChecksum(Algorithm.SHA1, UUID.randomUUID().toString());
+        }
+        return fileInfo;
     }
 
     public Node getRoot() {
