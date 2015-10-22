ab8ca70d72e5916d848265afd651782ca64494af
Mark Patton <mark.patton@gmail.com>
2015-10-22 14:34:57 -0400

    Add more ipm trees for testing.

--- dcs-packaging-tool-impl/src/test/java/org/dataconservancy/packaging/tool/impl/FarmIpmFactory.java
+++ dcs-packaging-tool-impl/src/test/java/org/dataconservancy/packaging/tool/impl/FarmIpmFactory.java
@@ -19,6 +19,50 @@ public class FarmIpmFactory {
     }
 
     /**
+     * Return a tree with types assigned of a single directory.
+     * 
+     * <pre>
+     *  /bestfarm                (Farm)
+     * </pre>
+     * 
+     * @return root of tree.
+     */
+    public Node createSingleDirectoryTree() {
+        Node root = new Node(URI.create("test:bestfarm"));
+
+        root.setNodeType(profile.getFarmNodeType());
+        root.setFileInfo(createFileInfo("/bestfarm", "best farm", -1, 10123, 100, false));
+
+        return root;
+    }
+
+    /**
+     * Return a tree with types assigned of a single directory with a single
+     * subdirectory.
+     * 
+     * <pre>
+     *  /farm                (Farm)
+     *  /far/barn/           (Barn)
+     * </pre>
+     * 
+     * @return root of tree.
+     */
+    public Node createTwoDirectoryTree() {
+        Node root = new Node(URI.create("test:farm"));
+
+        root.setNodeType(profile.getFarmNodeType());
+        root.setFileInfo(createFileInfo("/farm", "farm", -1, 10123, 100, false));
+
+        Node barn = new Node(URI.create("test:barn"));
+        barn.setNodeType(profile.getBarnNodeType());
+        barn.setFileInfo(createFileInfo("/farm/barn", "barn", -1, 200, 400, false));
+
+        root.addChild(barn);
+
+        return root;
+    }
+
+    /**
      * Return a tree with types assigned.
      * 
      * <pre>
@@ -27,6 +71,7 @@ public class FarmIpmFactory {
      *  /farm/barn1/cow1 (Cow)
      *  /farm/barn1/cow1/lastgoodbye.mp4 (Media)
      * </pre>
+     * 
      * @return root of tree.
      */
     public Node createSimpleTree() {
@@ -38,23 +83,64 @@ public class FarmIpmFactory {
         Node barn = new Node(URI.create("test:barn1"));
         barn.setNodeType(profile.getBarnNodeType());
         barn.setFileInfo(createFileInfo("/farm/barn1", "barn1", -1, 200, 400, false));
-        
+
         Node cow = new Node(URI.create("test:cow1"));
         cow.setNodeType(profile.getCowNodeType());
         cow.setFileInfo(createFileInfo("/farm/barn1/cow1", "cow1", -1, 600, 600, false));
-        
+
         Node video = new Node(URI.create("test:cow1_video"));
         video.setNodeType(profile.getMediaNodeType());
-        video.setFileInfo(createFileInfo("/farm/barn1/cow1/lastgoodbye.mp4", "lastgoodbye.mp4", 10000000, 800, 1000, true));
-        
+        video.setFileInfo(
+                createFileInfo("/farm/barn1/cow1/lastgoodbye.mp4", "lastgoodbye.mp4", 10000000, 800, 1000, true));
+
         root.addChild(barn);
         barn.addChild(cow);
         cow.addChild(video);
-        
+
         return root;
     }
 
-    private FileInfo createFileInfo(String path, String name, final long size, final long create_date_ms, final long mod_date_ms, boolean file) {
+    /**
+     * Return a tree with types assigned.
+     * 
+     * <pre>
+     *  /farm                (Farm)
+     *  /farm/sounds.mp3     (Media)
+     *  /farm/barn/           (Barn)
+     *  /farm/barn/photo.jpg (Media)
+     * </pre>
+     * 
+     * @return root of tree.
+     */
+    public Node createSimpleTree2() {
+        Node root = new Node(URI.create("test:farm"));
+
+        root.setNodeType(profile.getFarmNodeType());
+        root.setFileInfo(createFileInfo("/farm", "farm", -1, 0, 100, false));
+
+        Node barn = new Node(URI.create("test:barn"));
+        barn.setNodeType(profile.getBarnNodeType());
+        barn.setFileInfo(createFileInfo("/farm/barn", "barn1", -1, 200, 400, false));
+
+        Node sounds = new Node(URI.create("test:farm_sounds"));
+        sounds.setNodeType(profile.getMediaNodeType());
+        sounds.setFileInfo(
+                createFileInfo("/farm/sounds.mp3", "sounds.mp3", 1300000, 8000, 19000, true));
+        
+        Node photo  = new Node(URI.create("test:barn_photo"));
+        photo.setNodeType(profile.getMediaNodeType());
+        photo.setFileInfo(
+                createFileInfo("/farm/barn/photo.jpg", "photo.jpg", 300000, 80000, 100000, true));
+
+        root.addChild(barn);
+        root.addChild(sounds);
+        barn.addChild(photo);
+
+        return root;
+    }
+
+    private FileInfo createFileInfo(String path, String name, final long size, final long create_date_ms,
+            final long mod_date_ms, boolean file) {
         FileInfo fileInfo = new FileInfo(Paths.get(path).toUri(), name);
         fileInfo.setSize(size);
         fileInfo.setCreationTime(FileTime.fromMillis(create_date_ms));
