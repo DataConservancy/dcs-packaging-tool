c8bdaadeb957a9c0db0f9768e40b35359643fc3e
bbrosius <benbrosius@gmail.com>
2015-10-30 14:13:25 -0400

    Added tests to make sure Domain Objects properties all validate.

--- dcs-packaging-tool-profiles/dcs-packaging-tool-profile-dcs/src/test/java/org/dataconservancy/packaging/tool/profile/DcBoProfileServiceTest.java
+++ dcs-packaging-tool-profiles/dcs-packaging-tool-profile-dcs/src/test/java/org/dataconservancy/packaging/tool/profile/DcBoProfileServiceTest.java
@@ -8,9 +8,14 @@ import org.dataconservancy.packaging.tool.impl.DomainProfileServiceImpl;
 import org.dataconservancy.packaging.tool.impl.SimpleURIGenerator;
 import org.dataconservancy.packaging.tool.impl.URIGenerator;
 import org.dataconservancy.packaging.tool.impl.support.IpmTreeFactory;
+import org.dataconservancy.packaging.tool.model.dprofile.Property;
 import org.dataconservancy.packaging.tool.model.ipm.Node;
+import org.joda.time.DateTime;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.net.URI;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -114,8 +119,194 @@ public class DcBoProfileServiceTest {
     }
 
     @Test
+    @Ignore("Unignore once transforms are updated")
     public void testDataItemToSubCollectionTransformWithFile() {
+        Node root = boIpmFactory.createSmallLinearTree();
+        root.walk(store::updateObject);
 
+        Node dataItem = root.getChildren().get(0).getChildren().get(0);
+        service.transformNode(dataItem, profile.getDataItemToCollectionTransform());
+
+        assertEquals(profile.getCollectionNodeType().getIdentifier(), dataItem.getNodeType().getIdentifier());
+        assertEquals(root.getChildren().get(0).getIdentifier(), dataItem.getParent().getIdentifier());
+
+        assertEquals(profile.getMetadataNodeType(), dataItem.getChildren().get(0).getNodeType().getIdentifier());
+        assertTrue(service.validateTree(root));
+    }
+
+    /**
+     * Test validating properties on a Project
+     */
+    @Test
+    public void testValidateProjectProperties() {
+        Node node = new Node(URI.create("test:node"));
+        node.setNodeType(profile.getProjectNodeType());
+        node.setDomainObject(URI.create("domain:object"));
+
+        // Missing species, title, and weight
+        assertFalse(service.validateProperties(node, profile.getProjectNodeType()));
+
+        Property title = new Property(profile.getHasTitle());
+        title.setStringValue("title");
+        service.addProperty(node, title);
+
+        Property description = new Property(profile.getHasDescription());
+        description.setStringValue("description");
+        service.addProperty(node, description);
+
+        Property startDate = new Property(profile.getHasStartDate());
+        startDate.setDateTimeValue(new DateTime());
+        service.addProperty(node, startDate);
+
+        Property hasAlottedStorage = new Property(profile.getHasAlottedStorage());
+        hasAlottedStorage.setLongValue(1000L);
+        service.addProperty(node, hasAlottedStorage);
+
+        Property hasUsedStorage = new Property(profile.getHasUsedStorage());
+        hasUsedStorage.setLongValue(50L);
+        service.addProperty(node, hasUsedStorage);
+
+        Property hasPublisher = new Property(profile.getHasPublisher());
+        hasPublisher.setStringValue("publisher");
+        service.addProperty(node, hasPublisher);
+
+        assertTrue(service.validateProperties(node, profile.getProjectNodeType()));
+    }
+
+    /**
+     * Test validating properties on a Project
+     */
+    @Test
+    public void testValidateCollectionProperties() {
+        Node node = new Node(URI.create("test:node"));
+        node.setNodeType(profile.getCollectionNodeType());
+        node.setDomainObject(URI.create("domain:object"));
+
+        // Missing species, title, and weight
+        assertFalse(service.validateProperties(node, profile.getCollectionNodeType()));
+
+        Property title = new Property(profile.getHasTitle());
+        title.setStringValue("title");
+        service.addProperty(node, title);
+
+        Property description = new Property(profile.getHasDescription());
+        description.setStringValue("description");
+        service.addProperty(node, description);
+
+        Property createDate = new Property(profile.getHasCreateDate());
+        createDate.setDateTimeValue(new DateTime());
+        service.addProperty(node, createDate);
+
+        assertTrue(service.validateProperties(node, profile.getCollectionNodeType()));
+    }
+
+    /**
+     * Test validating properties on a DataItem
+     */
+    @Test
+    public void testValidateDataItemProperties() {
+        Node node = new Node(URI.create("test:node"));
+        node.setNodeType(profile.getDataItemNodeType());
+        node.setDomainObject(URI.create("domain:object"));
+
+        // Missing species, title, and weight
+        assertFalse(service.validateProperties(node, profile.getDataItemNodeType()));
+
+        Property title = new Property(profile.getHasTitle());
+        title.setStringValue("title");
+        service.addProperty(node, title);
+
+        Property description = new Property(profile.getHasDescription());
+        description.setStringValue("description");
+        service.addProperty(node, description);
+
+        Property createDate = new Property(profile.getHasCreateDate());
+        createDate.setDateTimeValue(new DateTime());
+        service.addProperty(node, createDate);
+
+        Property modifiedDate = new Property(profile.getHasModifiedDate());
+        modifiedDate.setDateTimeValue(new DateTime());
+        service.addProperty(node, modifiedDate);
+
+        assertTrue(service.validateProperties(node, profile.getDataItemNodeType()));
+    }
+
+    /**
+     * Test validating properties on a File
+     */
+    @Test
+    public void testValidateFileProperties() {
+        Node node = new Node(URI.create("test:node"));
+        node.setNodeType(profile.getFileNodeType());
+        node.setDomainObject(URI.create("domain:object"));
+
+        // Missing species, title, and weight
+        assertFalse(service.validateProperties(node, profile.getFileNodeType()));
+
+        Property title = new Property(profile.getHasTitle());
+        title.setStringValue("title");
+        service.addProperty(node, title);
+
+        Property description = new Property(profile.getHasDescription());
+        description.setStringValue("description");
+        service.addProperty(node, description);
+
+        Property createDate = new Property(profile.getHasCreateDate());
+        createDate.setDateTimeValue(new DateTime());
+        service.addProperty(node, createDate);
+
+        Property modifiedDate = new Property(profile.getHasModifiedDate());
+        modifiedDate.setDateTimeValue(new DateTime());
+        service.addProperty(node, modifiedDate);
+
+        Property format = new Property(profile.getHasFormat());
+        format.setStringValue("application/xml");
+        service.addProperty(node, format);
+
+        Property size = new Property(profile.getHasSize());
+        size.setLongValue(1234L);
+        service.addProperty(node, size);
+
+        assertTrue(service.validateProperties(node, profile.getFileNodeType()));
+    }
+
+    /**
+     * Test validating properties on a Metadata
+     */
+    @Test
+    public void testValidateMetadataProperties() {
+        Node node = new Node(URI.create("test:node"));
+        node.setNodeType(profile.getMetadataNodeType());
+        node.setDomainObject(URI.create("domain:object"));
+
+        // Missing species, title, and weight
+        assertFalse(service.validateProperties(node, profile.getMetadataNodeType()));
+
+        Property title = new Property(profile.getHasTitle());
+        title.setStringValue("title");
+        service.addProperty(node, title);
+
+        Property description = new Property(profile.getHasDescription());
+        description.setStringValue("description");
+        service.addProperty(node, description);
+
+        Property createDate = new Property(profile.getHasCreateDate());
+        createDate.setDateTimeValue(new DateTime());
+        service.addProperty(node, createDate);
+
+        Property modifiedDate = new Property(profile.getHasModifiedDate());
+        modifiedDate.setDateTimeValue(new DateTime());
+        service.addProperty(node, modifiedDate);
+
+        Property format = new Property(profile.getHasFormat());
+        format.setStringValue("application/xml");
+        service.addProperty(node, format);
+
+        Property size = new Property(profile.getHasSize());
+        size.setLongValue(1234L);
+        service.addProperty(node, size);
+
+        assertTrue(service.validateProperties(node, profile.getMetadataNodeType()));
     }
 
     // Update objects and then validate the tree
