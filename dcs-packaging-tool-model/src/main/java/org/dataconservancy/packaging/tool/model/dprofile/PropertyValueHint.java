4dbae6ee07274a31feefadcd47fa88b8fc70dad9
Aaron Birkland <apb18@cornell.edu>
2015-11-11 12:03:35 -0500

    Fix broken build

--- dcs-packaging-tool-model/src/main/java/org/dataconservancy/packaging/tool/model/dprofile/PropertyValueHint.java
+++ dcs-packaging-tool-model/src/main/java/org/dataconservancy/packaging/tool/model/dprofile/PropertyValueHint.java
@@ -4,5 +4,5 @@ package org.dataconservancy.packaging.tool.model.dprofile;
  * Hints about how to display and interpret property values.
  */
 public enum PropertyValueHint {
-    TEXT, PHONE_NUMBER, NUMBER, EMAIL, PERSON_NAME, FILE_NAME, URL, CONTACT_INFO, FILE_SIZE, MULTI_LINE_TEXT, DCS_DISCIPLINE
+    TEXT, PHONE_NUMBER, NUMBER, EMAIL, PERSON_NAME, FILE_NAME, URL, CONTACT_INFO, FILE_SIZE, MULTI_LINE_TEXT, DCS_DISCIPLINE, DATE_TIME
 }
