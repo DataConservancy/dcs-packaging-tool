b08ed74d5e7852e52a0e275126a9613e6c60d6b6
Elliot Metsger <emetsger@jhu.edu>
2015-11-13 00:29:43 -0500

    DC-2129: Fix missing import!

--- dcs-packaging-tool-impl/src/test/java/org/dataconservancy/packaging/tool/impl/AnnotationDrivenPackageStateSerializerTest.java
+++ dcs-packaging-tool-impl/src/test/java/org/dataconservancy/packaging/tool/impl/AnnotationDrivenPackageStateSerializerTest.java
@@ -26,6 +26,7 @@ import org.apache.commons.compress.archivers.ArchiveOutputStream;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+import org.apache.commons.compress.archivers.zip.ZipLong;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.NullOutputStream;
