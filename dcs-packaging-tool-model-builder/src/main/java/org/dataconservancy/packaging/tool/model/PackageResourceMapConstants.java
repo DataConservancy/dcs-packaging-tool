119ea3ce8fe7984e1e0a47e60815c556c6b9c7fd
Elliot Metsger <emetsger@jhu.edu>
2015-11-19 10:33:11 -0500

    DC-2148: Removing unused constants from PackageResourceMapConstants.

--- dcs-packaging-tool-model-builder/src/main/java/org/dataconservancy/packaging/tool/model/PackageResourceMapConstants.java
+++ dcs-packaging-tool-model-builder/src/main/java/org/dataconservancy/packaging/tool/model/PackageResourceMapConstants.java
@@ -20,42 +20,12 @@ public interface PackageResourceMapConstants {
     
     public static final String DATACONS_NS_URI = "http://dataconservancy.org/ns/types/";
     
-    public static final Resource PACKAGE_DESCRIPTION_TYPE = ResourceFactory
-            .createResource(DATACONS_NS_URI + "PackageDescription");
-    
-    public static final Resource ARTIFACT_TYPE = ResourceFactory
-            .createResource(DATACONS_NS_URI + "Artifact");
-    
-    public static final Resource PROPERTY_TYPE = ResourceFactory
-            .createResource(DATACONS_NS_URI + "Property");
-    
-    public static final Resource RELATIONSHIP_TYPE = ResourceFactory
-            .createProperty(DATACONS_NS_URI, "Relationship");
-
-    public static final Property HAS_ARTIFACT = ResourceFactory.createProperty(DATACONS_NS_URI, "hasArtifact");
-    
-    public static final Property HAS_SPECIFICATION_ID = ResourceFactory.createProperty(DATACONS_NS_URI, "hasSpecificationId");
-    
-    public static final Property HAS_TYPE = ResourceFactory.createProperty(DATACONS_NS_URI, "hasType");
-    
-    public static final Property HAS_REF = ResourceFactory.createProperty(DATACONS_NS_URI, "hasRef");
-    
     public static final Property HAS_ID = ResourceFactory.createProperty(DATACONS_NS_URI, "hasId");
     
-    public static final Property HAS_PROPERTY = ResourceFactory.createProperty(DATACONS_NS_URI, "hasProperty");
-    
     public static final Property HAS_NAME = ResourceFactory.createProperty(DATACONS_NS_URI, "hasName");
-    
-    public static final Property HAS_VALUE = ResourceFactory.createProperty(DATACONS_NS_URI, "hasValue");
-    
-    public static final Property HAS_RELATIONSHIP = ResourceFactory.createProperty(DATACONS_NS_URI, "hasRelationship");
-    
-    public static final Property HAS_TARGET = ResourceFactory.createProperty(DATACONS_NS_URI, "hasTarget");
 
     public static final Property IS_BYTE_STREAM = ResourceFactory.createProperty(DATACONS_NS_URI, "isByteStream");
     
     public static final Property IS_IGNORED = ResourceFactory.createProperty(DATACONS_NS_URI, "isIgnored");
-
-    public static final Property REQUIRES_URI = ResourceFactory.createProperty(DATACONS_NS_URI, "requiresURI");
 
 }
