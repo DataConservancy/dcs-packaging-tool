105abb7cf366a80598ada696e4b501aca26fb6c7
Elliot Metsger <emetsger@jhu.edu>
2015-11-19 10:32:41 -0500

    DC-2148: Removing indiscriminate dependency on jena-libs from dcs-shared-util.  Adding dependency on jena-core to -model.  Moving ResourceMapConstants and ResourceMapUtil to -packaging-tool-impl.

--- dcs-packaging-tool-impl/src/test/java/org/dataconservancy/packaging/tool/support/ResourceMapUtil.java
+++ dcs-packaging-tool-impl/src/test/java/org/dataconservancy/packaging/tool/support/ResourceMapUtil.java
@@ -1,19 +1,21 @@
 /*
- * Copyright 2013 Johns Hopkins University
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2015 Johns Hopkins University
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
-package org.dataconservancy.packaging.shared;
+package org.dataconservancy.packaging.tool.support;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -22,6 +24,7 @@ import org.apache.jena.rdf.model.Selector;
 import org.apache.jena.rdf.model.SimpleSelector;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
+import org.dataconservancy.packaging.shared.PackageException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
