1b10c9bc1a3f90a5d2e19ec157c1b1bb2fceef13
Aaron Birkland <apb@jhu.edu>
2015-12-01 08:04:53 -0500

    DC-2095: Add node transforms and refreshes to package generation IT

--- dcs-packaging-tool-integration/src/test/java/org/dataconservancy/packaging/tool/integration/PackageGenerationTest.java
+++ dcs-packaging-tool-integration/src/test/java/org/dataconservancy/packaging/tool/integration/PackageGenerationTest.java
@@ -17,13 +17,16 @@
 package org.dataconservancy.packaging.tool.integration;
 
 import java.io.File;
+import java.io.IOException;
 
 import java.net.URI;
 
 import java.nio.file.Paths;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.SimpleSelector;
@@ -36,19 +39,25 @@ import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 
 import org.dataconservancy.packaging.tool.api.DomainProfileService;
+import org.dataconservancy.packaging.tool.api.IPMService;
+import org.dataconservancy.packaging.tool.api.support.NodeComparison;
 import org.dataconservancy.packaging.tool.impl.IpmRdfTransformService;
 import org.dataconservancy.packaging.tool.model.OpenedPackage;
 import org.dataconservancy.packaging.tool.model.PackageState;
 import org.dataconservancy.packaging.tool.model.RDFTransformException;
+import org.dataconservancy.packaging.tool.model.dprofile.NodeTransform;
+import org.dataconservancy.packaging.tool.model.ipm.Node;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.dataconservancy.packaging.tool.impl.generator.RdfUtil.copy;
 import static org.dataconservancy.packaging.tool.impl.generator.RdfUtil.cut;
 import static org.dataconservancy.packaging.tool.impl.generator.RdfUtil.selectLocal;
+import static org.dataconservancy.packaging.tool.ontologies.Ontologies.NS_DCS_ONTOLOGY_BOM;
 
 @ContextConfiguration({
         "classpath*:org/dataconservancy/config/applicationContext.xml",
@@ -81,40 +90,45 @@ public class PackageGenerationTest {
     @Autowired
     public IpmRdfTransformService ipm2rdf;
 
+    @Autowired
+    public IPMService ipmService;
+
     @Test
-    public void fileExistsTest() throws Exception {
+    public void fileLocationTest() throws Exception {
 
         PackageState state = initializer.initialize(DCS_PROFILE);
 
-        OpenedPackage opened = packager.createPackage(state, folder.getRoot());
+        Set<URI> originalFileLocations = new HashSet<>();
 
-        opened.getPackageTree().walk(node -> {
-            if (node.getFileInfo() != null && node.getFileInfo().isFile()) {
-                File file =
-                        Paths.get(node.getFileInfo().getLocation()).toFile();
-                assertTrue(file.exists());
-                assertTrue(file.isFile());
-            }
-        });
-    }
-
-    @Test
-    public void directoriesExistTest() throws Exception {
-        PackageState state = initializer.initialize(DCS_PROFILE);
+        ipm2rdf.transformToNode(state.getPackageTree())
+                .walk(node -> originalFileLocations.add(node.getFileInfo()
+                        .getLocation()));
 
         OpenedPackage opened = packager.createPackage(state, folder.getRoot());
 
         opened.getPackageTree()
                 .walk(node -> {
                     if (node.getFileInfo() != null
+                            && node.getFileInfo().isFile()) {
+                        File file =
+                                Paths.get(node.getFileInfo().getLocation())
+                                        .toFile();
+                        assertTrue(file.exists());
+                        assertTrue(file.isFile());
+                        assertFalse(originalFileLocations.contains(node
+                                .getFileInfo().getLocation()));
+                    } else if (node.getFileInfo() != null
                             && node.getFileInfo().isDirectory()) {
                         File dir =
                                 Paths.get(node.getFileInfo().getLocation())
                                         .toFile();
                         assertTrue(dir.exists());
                         assertTrue(dir.isDirectory());
-
                     }
+
+                    assertFalse(originalFileLocations.contains(node
+                            .getFileInfo().getLocation()));
+
                 });
     }
 
@@ -170,6 +184,134 @@ public class PackageGenerationTest {
                 .createPackage(opened.getPackageState(), folder.getRoot())
                 .getPackageState()));
 
+    }
+
+    @Test
+    public void transformTest() throws Exception {
+
+        PackageState state = initializer.initialize(DCS_PROFILE);
+        OpenedPackage opened = packager.createPackage(state, folder.getRoot());
+
+        DomainProfileService profileService =
+                profileServiceFactory.getProfileService(opened
+                        .getPackageState().getDomainObjectRDF());
+
+        Map<Node, NodeTransform> toTransform = new HashMap<>();
+
+        opened.getPackageTree().walk(node -> {
+
+            /* Transform all Metadata to DI + File */
+            if (node.getNodeType().getDomainTypes()
+                    .contains(URI.create(NS_DCS_ONTOLOGY_BOM + "Metadata"))) {
+                profileService
+                        .getNodeTransforms(node)
+                        .stream()
+                        .filter(xform -> xform
+                                .getResultNodeType()
+                                .getDomainTypes()
+                                .contains(URI.create(NS_DCS_ONTOLOGY_BOM
+                                        + "File")))
+                        .forEach(xform -> toTransform.put(node, xform));
+            }
+        });
+
+        toTransform.forEach(profileService::transformNode);
+
+        opened.getPackageState().setPackageTree(ipm2rdf.transformToRDF(opened
+                .getPackageTree()));
+
+        OpenedPackage openedAfterTransform =
+                packager.createPackage(opened.getPackageState(),
+                                       folder.getRoot());
+
+        assertEquals(domainObjectSizes(opened.getPackageState()),
+                     domainObjectSizes(openedAfterTransform.getPackageState()));
+
+    }
+
+    /*
+     * Verify that when 'refreshed' after opening from a package, no IPM tree
+     * changes are suggested.
+     */
+    @Test
+    public void refreshTest() throws Exception {
+        PackageState state = initializer.initialize(DCS_PROFILE);
+        OpenedPackage opened = packager.createPackage(state, folder.getRoot());
+
+        Node rescannedTree =
+                ipmService.createTreeFromFileSystem(Paths.get(opened
+                        .getPackageTree().getFileInfo().getLocation()));
+        buildContentRoots(opened.getPackageTree(), rescannedTree);
+        Map<Node, NodeComparison> comparisons =
+                ipmService.compareTree(opened.getPackageTree(), rescannedTree);
+
+        assertTrue(comparisons.isEmpty());
+    }
+
+    /*
+     * TODO: Copied verbatim from EditPackageContentPresenterImpl - maybe these
+     * generic tree operations should be in a common library?
+     */
+    private void buildContentRoots(Node node, Node newTree) throws IOException {
+        if (node.getChildren() != null) {
+            for (Node child : node.getChildren()) {
+                if (child.getFileInfo() != null
+                        && Paths.get(child.getFileInfo().getLocation())
+                                .toFile().exists()) {
+                    if (!Paths.get(child.getFileInfo().getLocation())
+                            .startsWith(Paths.get(node.getFileInfo()
+                                    .getLocation()))) {
+                        Node newTreeParent =
+                                getNewTreeNodeForExistingNode(node, newTree);
+                        if (newTreeParent != null) {
+                            newTreeParent.addChild(buildComparisonTree(child));
+                        } else {
+                            newTree.addChild(buildComparisonTree(child));
+                        }
+                    } else if (child.getChildren() != null) {
+                        buildContentRoots(child, newTree);
+                    }
+                } else if (child.getChildren() != null) {
+                    buildContentRoots(child, newTree);
+                }
+            }
+        }
+    }
+
+    /*
+     * TODO: Copied verbatim from EditPackageContentPresenterImpl - maybe these
+     * generic tree operations should be in a common library?
+     */
+    private Node buildComparisonTree(Node node) throws IOException {
+        Node newTree =
+                ipmService.createTreeFromFileSystem(Paths.get(node
+                        .getFileInfo().getLocation()));
+        buildContentRoots(node, newTree);
+
+        return newTree;
+    }
+
+    /*
+     * TODO: Copied verbatim from EditPackageContentPresenterImpl - maybe these
+     * generic tree operations should be in a common library?
+     */
+    private Node getNewTreeNodeForExistingNode(Node node, Node newTree) {
+        Node foundNode = null;
+        if (node.getFileInfo() != null
+                && newTree.getFileInfo() != null
+                && node.getFileInfo().getLocation()
+                        .equals(newTree.getFileInfo().getLocation())) {
+            foundNode = newTree;
+        } else if (newTree.getChildren() != null) {
+            for (Node newTreeChild : newTree.getChildren()) {
+                foundNode = getNewTreeNodeForExistingNode(node, newTreeChild);
+                if (foundNode != null) {
+                    break;
+                }
+            }
+        }
+
+        return foundNode;
     }
 
     private Map<URI, Integer> domainObjectSizes(PackageState state)
